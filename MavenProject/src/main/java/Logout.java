012dfe4b9803479d5ca0fe0fd4ff50f13c729a4a
hp <hp@DESKTOP-V4C1LNR>
2019-01-22 15:02:24 +0530

    first push

---
+++ MavenProject/src/main/java/Logout.java
@@ -0,0 +1,14 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Logout {
+	WebDriver driver;
+	By logout = By.linkText("log out");
+	public Logout() {
+		super();
+	}
+	
+
+}
---
+++ MavenProject/src/test/java/MagentoTest.java
@@ -0,0 +1,27 @@
+
+
+	import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+	public class MagentoTest
+	{
+		@Test
+		public void magento() {
+			
+		String url ="http//www.magento.com";
+		
+			WebDriver driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			driver.get(url); 
+			Home h =new Home (driver);
+			h.clickOnMyAcc();
+			Login l1 =new Login (driver);
+			l1.typeEmail("nithinmanjunath1991@gmail.com");
+			l1.typepassword(" welcome123");
+			l1.cliclOnLogin();
+			
+	}
+	}
